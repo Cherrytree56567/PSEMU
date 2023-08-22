37644726882c6b5a5780892262ba0899816a33c0
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-08-22 18:36:54 +1000

    Update CPU.h

--- PSEMU/CPU.h
+++ PSEMU/CPU.h
@@ -8,12 +8,36 @@ public:
         // Initialize any other settings or variables
     }
 
+    void op_add(uint32_t instruction, CPURegisters& registers, int currentbit) {
+        uint8_t rs = extractField(instruction, currentbit+, 25);
+        uint8_t rt = extractField(instruction, 16, 20);
+        uint8_t rd = extractField(instruction, 11, 15);
+
+        uint32_t rsValue = registers.reg[rs];
+        uint32_t rtValue = registers.reg[rt];
+
+        // Perform the add operation
+        uint32_t result = rsValue + rtValue;
+
+        // Store the result in the destination register
+        registers.reg[rd] = result;
+
+        // Update program counter
+        registers.pc += 4;
+    }
+
+    void loadInstructions(const char* binaryCode, size_t size) {
+        // Copy binary code into memory starting at address 0x0
+        memcpy(memory.getMemoryPointer(), binaryCode, size);
+    }
+
     void run() {
         while (true) {
             // Fetch opcode from memory at PC
+            int currentbit = 0;
             uint32_t instruction = memory.readWord(registers.pc);
-            uint8_t opcode = extractField(instruction, 0, 5);
-            uint8_t funct = extractField(instruction, 26, 31);
+            uint8_t opcode = extractField(instruction, currentbit, currentbit+5);
+            uint8_t funct = extractField(instruction, currentbit+31-6, currentbit+31);
 
             switch (opcode) {
                 case 0b000000: // R-type instructions
@@ -23,28 +47,122 @@ public:
                             break;
 
                         case 0b100001:
-                            // addu
+                            // Addu
+                            break;
+
+                        case 0b100100:
+                            // and
+                            break;
+
+                        case 0b011010:
+                            // div
+                            break;
+
+                        case 0b011011:
+                            // divu
+                            break;
+
+                        case 0b001001:
+                            // jalr
+                            break;
+
+                        case 0b001000:
+                            // jr
+                            break;
+
+                        case 0b010000:
+                            // mfhi
+                            break;
+
+                        case 0b010001:
+                            // mthi
+                            break;
+
+                        case 0b010010:
+                            // mflo
+                            break;
+
+                        case 0b010011:
+                            // mtlo
+                            break;
+
+                        case 0b011000:
+                            // mult
+                            break;
+
+                        case 0b011001:
+                            // multu
+                            break;
+
+                        case 0b100111:
+                            // nor
+                            break;
+
+                        case 0b100110:
+                            // xor
+                            break;
+
+                        case 0b100101:
+                            // or
+                            break;
+
+                        case 0b101010:
+                            // slt
+                            break;
+
+                        case 0b101011:
+                            // sltu
+                            break;
+
+                        case 0b000000:
+                            // sll
+                            break;
+
+                        case 0b000010:
+                            // srl
+                            break;
+
+                        case 0b000011:
+                            // sra
+                            break;
+
+                        case 0b100010:
+                            // sub
+                            break;
+
+                        case 0b100011:
+                            // subu
                             break;
 
                         default:
                             // Handle invalid funct code
+                            Logging console;
+                            console.warn("Invalid Function code");
                             break;
                     }
                     break;
 
+                // J Type Instructions
+                case 0b000010:
+                    // j
+                    break;
+
+                case 0b000011:
+                    // jal
+                    break;
+
+                // I Type Instructions
                 case 0b001000:
-                    // Addi
+                    // addi
                     break;
 
                 case 0b001001:
-                    // Addiu
+                    // addiu
                     break;
 
                 case 0b001100:
-                    // Andi
+                    // andi
                     break;
-
-                // Add more cases for other I-type instructions...
 
                 case 0b000100:
                     // beq
@@ -58,28 +176,57 @@ public:
                     // bne
                     break;
 
-                // Add more cases for other I-type instructions...
-
-                case 0b000010:
-                    // j
+                case 0b000111:
+                    // bgtz
                     break;
 
-                case 0b000011:
-                    // jal
-                    break;
-
-                // Add more cases for other J-type instructions...
-
-                case 0b100000: // lb
+                case 0b100000:
                     // lb
                     break;
 
-                case 0b100100: // lbu
+                case 0b100100:
                     // lbu
                     break;
 
+                case 0b100101:
+                    // lhu
+                    break;
+
+                case 0b001111:
+                    // lui
+                    break;
+
+                case 0b100011:
+                    // lw
+                    break;
+
+                case 0b001101:
+                    // ori
+                    break;
+
+                case 0b101000:
+                    // sb
+                    break;
+
+                case 0b101001:
+                    // sh
+                    break;
+
+                case 0b001010:
+                    // slti
+                    break;
+
+                case 0b001011:
+                    // sltiu
+                    break;
+
+                case 0b101011:
+                    // sw
+                    break;
+
                 default:
-                    // Handle invalid opcode
+                    Logging console;
+                    console.warn("Invalid Opcode");
                     break;
             }
 
