4edda4250a8ff619116501f9f85ccf836864f45e
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-09-13 17:58:00 +1000

    Update CPU.cpp

--- PSEMU/CPU.cpp
+++ PSEMU/CPU.cpp
@@ -14,10 +14,8 @@
 * Notes: 
 *        Warning: none of the op_* functions have been tested except the op_lui function.
 *        TODO: Fix the warning about opcode 51. it gives opcode 51 not found error even though there is no other instruction. I think it is something about the program not exiting properly.
-*        TODO: Test the op_add function
-*        TODO: Test the op_addi function
+*        TODO: Test the op_add, op_addi and op_addu function
 */
-
 
 // First Add rs and rt then store in rd (register)
 
@@ -156,6 +154,7 @@ void CPU::run() {
 
             case 0b100001:
                 // Addu
+                op_addu(instruction);
                 console.log("CPU INSTRUCTION :: ADDU");
                 break;
 
