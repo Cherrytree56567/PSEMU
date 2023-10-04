c62c42a54b066450248641421f67833d48ad4750
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-04 22:45:00 +1100

    update PSEMU/CPURegisters.h

--- PSEMU/CPURegisters.h
+++ PSEMU/CPURegisters.h
@@ -14,6 +14,7 @@
 class CPURegisters {
 public:
     uint32_t pc;   // Program Counter
+    uint32_t next_pc;
     uint32_t sp;   // Stack Pointer
     uint32_t reg[32];  // Array to hold all registers (including zero, at, v0-v1, a0-a3, t0-t9, s0-s7, k0-k1, gp, s8/fp, ra)
     uint32_t lo;   // LO register
