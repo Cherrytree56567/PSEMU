347a64251607a39d200c0bc7f27c622918244f60
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-06 22:26:52 +1100

    update PSEMU/CPU.h

--- PSEMU/CPU.h
+++ PSEMU/CPU.h
@@ -64,6 +64,7 @@ public:
     void op_sw(uint32_t instruction);
     void op_lh(uint32_t instruction);
     void op_break(uint32_t instruction);
+    void op_srlv(uint32_t instruction);
 
     void loadInstructions();
     void loadBiosCode(uint32_t* binaryCode, size_t numI);
