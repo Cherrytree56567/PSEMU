f98aa4c856a456ae613a10022348b57e9772ce20
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-08 11:18:24 +1100

    update PSEMU/Coprocessor.h

--- PSEMU/Coprocessor.h
+++ PSEMU/Coprocessor.h
@@ -10,72 +10,56 @@
 #include <fstream>
 #include <bitset>
 
-using uint = std::uint32_t;
-using ulong = std::uint64_t;
-using byte = std::int8_t;
-using ushort = std::uint16_t;
-
-union Cop0STAT {
-	uint raw;
-	struct {
-		uint IEc : 1;		/* Interrupt Enable (current) */
-		uint KUc : 1;		/* Kernel-User Mode (current) */
-		uint IEp : 1;		/* Interrupt Enable (previous) */
-		uint KUp : 1;		/* Kernel-User Mode (previous) */
-		uint IEo : 1;		/* Interrupt Enable (old) */
-		uint KUo : 1;		/* Kernel-User Mode (old) */
-		uint reserved1 : 2;
-		uint Sw : 2;		/* Software Interrupt Mask */
-		uint Intr : 6;	/* Hardware Interrupt Mask */
-		uint IsC : 1;		/* Isolate Cache */
-		uint reserved2 : 1;
-		uint PZ : 1;		/* Parity Zero */
-		uint reserved3 : 1;
-		uint PE : 1;		/* Parity Error */
-		uint TS : 1;		/* TLB Shutdown */
-		uint BEV : 1;		/* Bootstrap Exception Vectors */
-		uint reserved4 : 5;
-		uint Cu : 4;		/* Coprocessor Usability */
-	};
+class Coprocessor {
+public:
+    virtual uint32_t readRegister(uint8_t reg) = 0;
+    virtual void writeRegister(uint8_t reg, uint32_t value) = 0;
 };
 
-union Cop0CAUSE {
-	uint raw;
-	
-	struct {
-		uint : 2;
-		uint exc_code : 5;	/* Exception Code */
-		uint : 1;
-		uint Sw : 2;		/* Software Interrupts */
-		uint IP : 6;		/* Interrupt Pending */
-		uint : 12;
-		uint CE : 2;		/* Coprocessor Error */
-		uint BT : 1;		/* Branch Taken */
-		uint BD : 1;		/* Branch Delay */
-	};
+class Coprocessor0 : public Coprocessor {
+public:
+    enum C0Register {
+        STATUS = 12,
+        CAUSE = 13,
+        BAD_ADDRESS = 14,
+        EPC = 15
+    };
+
+    uint32_t readRegister(uint8_t reg) override {
+        switch (reg) {
+        case 12:
+            return status;
+        case 13:
+            return cause;
+        case 14:
+            return bad_address;
+        case 15:
+            return epc;
+        default:
+            return 0;
+        }
+    }
+
+    void writeRegister(uint8_t reg, uint32_t value) override {
+        switch (reg) {
+        case 12:
+            status = value;
+            break;
+        case 13:
+            cause = value;
+            break;
+        case 14:
+            bad_address = value;
+            break;
+        case 15:
+            epc = value;
+            break;
+        }
+    }
+
+private:
+    uint32_t status = 0;
+    uint32_t cause = 0;
+    uint32_t bad_address = 0;
+    uint32_t epc = 0;
 };
-
-union Cop0 {
-	uint regs[64];
-
-	struct {
-		uint r0;
-		uint r1;
-		uint r2;
-		uint Bpc;		/* Breakpoint Program Counter */
-		uint r4;
-		uint BDA;		/* Breakpoint Data Address */
-		uint TAR;		/* Target Address */
-		uint DCIC;		/* Debug and Cache Invalidate Control */
-		uint BadA;		/* Bad Address */
-		uint BDAM;		/* Breakpoint Data Address Mask */
-		uint r10;
-		uint BpcM;		/* Breakpoint Program Counter Mask */
-		Cop0STAT sr;	    /* Status */
-		Cop0CAUSE cause;	/* Cause */
-		uint epc;		/* Exception Program Counter */
-		uint PRId;		/* Processor Revision Identifier */
-		uint reserved[32];
-	};
-};
-
