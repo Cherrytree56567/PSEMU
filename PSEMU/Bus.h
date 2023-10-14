086e3bd3ff62914ceb4a3e1a9bd02c4c8ec0cd85
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-14 23:19:48 +1100

    update PSEMU/Bus.h

--- PSEMU/Bus.h
+++ PSEMU/Bus.h
@@ -7,13 +7,19 @@ class Bus {
 public:
 	Bus(int a) {}
 
-	uint32_t read32(uint32_t address) {
-		
-	}
+	uint32_t load32(uint32_t addr) {
+        if (auto offset = BIOS.contains(addr); offset.has_value()) {
+            return bios.load32(offset.value());
+        }
+
+        throw std::runtime_error("Unhandled fetch32 at address " + std::to_string(addr));
+    }
 
 	// RAM
 	static const int RAMSize = (2048 * 8000) / sizeof(uint8_t);
 	uint8_t RAM[RAMSize];
 	Bios bios;
+	
+const Range BIOS(0xbfc00000, 512 * 1024);
 };
 
