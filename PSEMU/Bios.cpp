bcf28bb8373a4626fcea23659644eebfaef5f64b
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-14 21:14:29 +1100

    update PSEMU/Bios.cpp

--- PSEMU/Bios.cpp
+++ PSEMU/Bios.cpp
@@ -12,14 +12,19 @@ void Bios::new(const char* path) {
     size_t file_size = file.tellg();
     file.seekg(0, std::ios::beg);
 
-    // Create a vector to store the data as uint8_t
-    std::vector<uint8_t> data(file_size);
-
     // Read the file into the vector
     file.read(reinterpret_cast<char*>(data.data()), file_size);
 
     if (!file) {
         std::cerr << "[Bios] Error: Failed to read the file." << std::endl;
     }
-    bdata.data = data;
+}
+
+void load32(uint32_t offset) {
+  uint32_t b0 = static_cast<uint32_t>(data[offset + 0]);
+    uint32_t b1 = static_cast<uint32_t>(data[offset + 1]);
+    uint32_t b2 = static_cast<uint32_t>(data[offset + 2]);
+    uint32_t b3 = static_cast<uint32_t>(data[offset + 3]);
+
+    uint32_t result = b0 | (b1 << 8) | (b2 << 16) | (b3 << 24);
 }
