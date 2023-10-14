6c85631d4eb456e486c960c2c05cc41b63c05811
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-14 22:59:22 +1100

    update PSEMU/Range.h

--- PSEMU/Range.h
+++ PSEMU/Range.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <iostream>
+#include <optional>
 
+struct Range {
+    uint32_t start;
+    uint32_t length;
+
+    Range(uint32_t s, uint32_t l) : start(s), length(l) {}
+
+    std::optional<uint32_t> contains(uint32_t addr) {
+        if (addr >= start && addr < start + length) {
+            return addr - start;
+        } else {
+            return std::nullopt;
+        }
+    }
+};
