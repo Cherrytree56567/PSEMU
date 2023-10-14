6229c67bb2ab59f6df5d367835c5f955282c8c6a
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-15 10:53:04 +1100

    update PSEMU/Bus.h

--- PSEMU/Bus.h
+++ PSEMU/Bus.h
@@ -9,7 +9,8 @@ public:
 	Bus(int a) {}
 
 	uint32_t load32(uint32_t addr) {
-        if (BIOS.contains(addr) != std::nullopt) {
+	  auto offset = BIOS.contains(addr);
+        if (offset != std::nullopt) {
             return bios.load32(offset.value());
         }
 
