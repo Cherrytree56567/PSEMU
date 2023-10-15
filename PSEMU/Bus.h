d3faad53bc5bb48b7119e71f6d8d4af82417f2bf
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-15 11:07:26 +1100

    update PSEMU/Bus.h

--- PSEMU/Bus.h
+++ PSEMU/Bus.h
@@ -6,11 +6,12 @@
 
 class Bus {
 public:
-	Bus(int a) {}
-	Range BIOSRange(0xbfc00000, 512 * 1024);
+    Bus(int a) : BIOSRange(0xbfc00000, 512 * 1024) {} // Initialize BIOSRange in the constructor
 
-	uint32_t load32(uint32_t addr) {
-	  auto offset = BIOSRange.contains(addr);
+    Range BIOSRange; // Declare BIOSRange as a member variable
+
+    uint32_t load32(uint32_t addr) {
+        auto offset = BIOSRange.contains(addr);
         if (offset != std::nullopt) {
             return bios.load32(offset.value());
         }
@@ -18,9 +19,8 @@ public:
         throw std::runtime_error("Unhandled fetch32 at address " + std::to_string(addr));
     }
 
-	// RAM
-	static const int RAMSize = (2048 * 8000) / sizeof(uint8_t);
-	uint8_t RAM[RAMSize];
-	Bios bios;
+    // RAM
+    static const int RAMSize = (2048 * 8000) / sizeof(uint8_t);
+    uint8_t RAM[RAMSize];
+    Bios bios;
 };
-
