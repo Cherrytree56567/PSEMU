c0d4b83f128e3170a477a24de57c989a9aa01902
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-15 20:46:20 +1100

    S

--- PSEMU/RAM.cpp
+++ PSEMU/RAM.cpp
@@ -7,25 +7,17 @@ void RAM::newl() {
 }
 
 uint32_t RAM::load32(uint32_t offset) {
-    /*uint8_t b0 = ram[offset + 0];
-    uint8_t b1 = ram[offset + 1];
-    uint8_t b2 = ram[offset + 2];
-    uint8_t b3 = ram[offset + 3];
-
-    return b0 | (b1 << 8) | (b2 << 16) | (b3 << 24);*/
-    std::cout << offset;
-    return (uint32_t)ram[offset];
+    /*uint32_t value = 0;
+    for (int i = 0; i < 4; ++i) {
+        value |= static_cast<uint32_t>(ram[offset + i]) << (8 * i);
+    }
+    return value;*/
+    return 0;
 }
 
 /// Store the 32bit little endian word `val` into `offset`
 void RAM::store32(uint32_t offset, uint32_t value) {
-    uint8_t b0 = value;
-    uint8_t b1 = (value >> 8);
-    uint8_t b2 = (value >> 16);
-    uint8_t b3 = (value >> 24);
-
-    ram[offset + 0] = b0;
-    ram[offset + 1] = b1;
-    ram[offset + 2] = b2;
-    ram[offset + 3] = b3;
+    /*for (int i = 0; i < 4; ++i) {
+        ram[offset + i] = static_cast<uint8_t>(value >> (8 * i));
+    }*/
 }
