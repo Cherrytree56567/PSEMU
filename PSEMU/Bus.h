3f6e257f37cc036c49bdde819572f8b986055538
Ronit Dsilva <124994670+Cherrytree56567@users.noreply.github.com>
2023-10-15 23:09:15 +1100

    update PSEMU/Bus.h

--- PSEMU/Bus.h
+++ PSEMU/Bus.h
@@ -16,15 +16,22 @@ public:
     const Range RAM_SIZE = Range(0x1f801060, 4);
     const Range CACHE_CONTROL = Range(0xfffe0130, 4);
     const Range RAM_ = Range(0x00000000, 2 * 1024 * 1024);
+    
+    uint32_t mask_region(uint32_t addr) {
+      // Index address space in 512MB chunks
+      uint32_t index = (addr >> 29);
+
+      return addr & region_mask[index];
+    }
 
     // Load and Store functions
     uint32_t load32(uint32_t addr) {
-        if (BIOS.contains(addr)) {
-            return bios.load32(BIOS.offset(addr));
-        } else if (RAM_SIZE.contains(addr)) {
+        if (BIOS.contains(mask_region(addr))) {
+            return bios.load32(BIOS.offset(mask_region(addr)));
+        } else if (RAM_SIZE.contains(mask_region(addr))) {
             return 0x00000888;
-        } else if (RAM_.contains(addr)) {
-            return ram.load32(RAM_.offset(addr));
+        } else if (RAM_.contains(mask_region(addr))) {
+            return ram.load32(RAM_.offset(mask_region(addr)));
         }
 
         if (addr % 4 != 0) {
@@ -35,15 +42,15 @@ public:
     }
 
     void store32(uint32_t addr, uint32_t value) {
-        if (SYS_CONTROL.contains(addr)) {
+        if (SYS_CONTROL.contains(mask_region(addr))) {
             return;
-        } else if (RAM_SIZE.contains(addr)) {
+        } else if (RAM_SIZE.contains(mask_region(addr))) {
             return;
         } else if (CACHE_CONTROL.contains(addr)) {
             std::cout << "[Bus] WARNING: Cache_Control not implemented.";
             return;
-        } else if (RAM_.contains(addr)) {
-            ram.store32(RAM_.offset((addr & region_mask[addr >> 29])), value);
+        } else if (RAM_.contains(mask_region(addr))) {
+            ram.store32(RAM_.offset(mask_region(addr), value);
             return;
         }
 
